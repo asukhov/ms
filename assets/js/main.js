383e6557329b37cc103b8a14148e0d03928b9c9e
asukhov <mila@iMac-Mila.local>
2016-11-04 17:39:13 +0300

    +

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,6 +80,32 @@
 					usePopupNav: true,
 					windowMargin: (skel.breakpoint('mobile').active ? 0 : 50)
 				});
+				$('#gallery4').poptrox({
+					//caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item a.image',
+					usePopupCaption: true,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (skel.breakpoint('mobile').active ? 0 : 50)
+				});
+				$('#gallery5').poptrox({
+					//caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item a.image',
+					usePopupCaption: true,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (skel.breakpoint('mobile').active ? 0 : 50)
+				});
 				
 				$body.removeClass('is-loading');
 			});
